ba3c2ebb04e2c878e42ac46d70b42ec541bf5e0c
breeze-yang <breeze@deshpro.com>
2019-03-08 10:58:05 +0800

    修改carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
 # 暂时不需要cdn
-# CarrierWave.configure do |config|
-#   if Rails.env.test?
-#     config.storage = :file
-#   else
-#     config.storage = :upyun
-#     config.upyun_username    = ENV['UPYUN_USERNAME']
-#     config.upyun_password    = ENV['UPYUN_PASSWD']
-#     config.upyun_bucket      = ENV['UPYUN_BUCKET']
-#     config.upyun_bucket_host = ENV['UPYUN_BUCKET_HOST']
-#   end
-# end
+CarrierWave.configure do |config|
+  config.storage = :file
+  config.base_path = ENV['ASSET_HOST']
+  # if Rails.env.test?
+  #   config.storage = :file
+  # else
+  #   config.storage = :upyun
+  #   config.upyun_username    = ENV['UPYUN_USERNAME']
+  #   config.upyun_password    = ENV['UPYUN_PASSWD']
+  #   config.upyun_bucket      = ENV['UPYUN_BUCKET']
+  #   config.upyun_bucket_host = ENV['UPYUN_BUCKET_HOST']
+  # end
+end
