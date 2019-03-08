dd55f5057cdb67d35917dab0e2be51fc64ca9500
Ricky <rickycxj@gmail.com>
2019-03-08 15:07:00 +0800

    冒号改成英文

--- app/services/account/vcode_service.rb
+++ app/services/account/vcode_service.rb
@@ -1,8 +1,8 @@
 module Services
   module Account
     class VcodeServices
-      COMMON_SMS_TEMPLATE = '【Pokerkinglive】Your verification code is：%s'.freeze
-      RESET_PWD_SMS_TEMPLATE = '【Pokerkinglive】Your verification code is：%s'.freeze
+      COMMON_SMS_TEMPLATE = '【Pokerkinglive】Your verification code is:%s'.freeze
+      RESET_PWD_SMS_TEMPLATE = '【Pokerkinglive】Your verification code is:%s'.freeze
       REGISTER_SMS_TITLE = '请激活您的帐号，完成注册'.freeze
       RESET_PWD_TITLE = '重设您的密码'.freeze
       COMMON_SMS_TITLE = 'Pokerkinglive验证码'.freeze
